40d71ebc42bd7a516a47e0ba57019f483adc1dbf
Abhinav <abhinavm347@gmail.com>
2025-10-16 07:46:21 +0530

    fixed vercel

--- backend/app.js
+++ backend/app.js
@@ -43,3 +43,4 @@ mongoose
 .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hbz3rtz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`)
 .then(()=>{app.listen(5000);})
 .catch(err=>console.log(err));
+module.exports = app;
